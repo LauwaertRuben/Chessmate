71785e446ca5aca55071d934f88e6f412c590c34
wouter.lenaerts <wouter.lenaerts1@student.kuleuven.be>
2018-11-10 18:31:07 +0100

    probeersel

--- probeersel.py
+++ probeersel.py
@@ -17,15 +17,20 @@ class chessman:
 
     def get_possible_moves(self, position):
         #stel we gaan er vanuit dat position een tuple is met de 2 coordinaten
-        if self.__kind == pion:
+        if self.__kind == "pion":
             #3 en 4 mogen enkel als hij een pion kan pakken van de tegenstander!
             option1 = (position[0], position[1] +1)
             option2 = (position[0], position[1] + 2)
             option3 = (position[0]-1, position[1] + 1)
             option4 = (position[0]+1, position[1] + 1)
-            return set(option1, option2, option3, option4)
+            return option1, option2, option3, option4
 
-
+        if self.__kind == "koning":
+            option1 = (position[0], position[1] + 1)
+            option2 = (position[0], position[1] -1)
+            option3 = (position[0]+1, position[1])
+            option4 = (position[0]-1, position[1])
+            return option1, option2, option3, option4
 
 
 
